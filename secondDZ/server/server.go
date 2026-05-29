6c1808d366bdf0f8715689e388d25c916e4e9de5
ArturMainet <eg.molotkov@gmail.com>
2026-05-30 00:57:08 +0300

    Complete_API_without_double

--- secondDZ/server/server.go
+++ secondDZ/server/server.go
@@ -1,76 +1,37 @@
 package main
 
 import (
-    "log"
-    "math/rand/v2"
-    "net/http"
-    "strconv"
+	"log"
+	"math/rand/v2"
+	"net/http"
+	"strconv"
 )
 
 func RandomHandler() http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        rand := rand.IntN(6) + 1
-        randStr := strconv.Itoa(rand)
-        w.WriteHeader(http.StatusOK)
-        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-        w.Write([]byte(randStr))
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		rand := rand.IntN(6) + 1
+		randStr := strconv.Itoa(rand)
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte(randStr))
+	}
 }
 
 func main() {
-    //creating router
-    //sadkaskjdjas
-    //aslkdmklasmdklasd
-    //askdmkasdmklaskldm
-    router := http.NewServeMux()
-    router.HandleFunc("/random", RandomHandler())
+	//creating router
+	//sadkaskjdjas
+	//aslkdmklasmdklasd
+	//askdmkasdmklaskldm
+	router := http.NewServeMux()
+	router.HandleFunc("/random", RandomHandler())
 
-    server := http.Server{
-        Addr:    ":8080",
-        Handler: router,
-    }
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
 
-    err := server.ListenAndServe()
-    if err != nil {
-        log.Fatalln(err)
-    }
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
-
-package main
-
-import (
-    "log"
-    "math/rand/v2"
-    "net/http"
-    "strconv"
-)
-
-func RandomHandler() http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        rand := rand.IntN(6) + 1
-        randStr := strconv.Itoa(rand)
-        w.WriteHeader(http.StatusOK)
-        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-        w.Write([]byte(randStr))
-    }
-}
-
-func main() {
-    //creating router
-    //sadkaskjdjas
-    //aslkdmklasmdklasd
-    //askdmkasdmklaskldm
-    router := http.NewServeMux()
-    router.HandleFunc("/random", RandomHandler())
-
-    server := http.Server{
-        Addr:    ":8080",
-        Handler: router,
-    }
-
-    err := server.ListenAndServe()
-    if err != nil {
-        log.Fatalln(err)
-    }
-}
-

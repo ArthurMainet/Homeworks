387b1cd029f1f1baea2b8064790b79c58b72ee05
ArturMainet <eg.molotkov@gmail.com>
2026-06-01 13:59:41 +0300

    docker_and_db

---
+++ 4-order-api/cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"Email-API/config"
+	"Email-API/internal/verify"
+	"log"
+	"net/http"
+)
+
+func main() {
+	conf := config.LoadConfig()
+	repo := verify.NewLocalRepo()
+
+	router := http.NewServeMux()
+	info := verify.NewEmailHandler(verify.EmailHandlerDeps{
+		Config: conf.EmailConf,
+		Repo:   repo,
+	})
+
+	router.HandleFunc("POST /send", info.ReciveEmail())
+	router.HandleFunc("GET /verify/{hash}", info.Verify())
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	log.Println("Starting to listen on port :8081")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}

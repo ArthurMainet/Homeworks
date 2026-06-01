d4e05b2891c20b023464c0595edac236d183fb51
ArturMainet <eg.molotkov@gmail.com>
2026-06-01 14:32:48 +0300

    added docker and DB

---
+++ 4-order-api/packages/DB/db.go
@@ -0,0 +1,22 @@
+package packages
+
+import (
+	"Email-API/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	*gorm.DB
+}
+
+func NewDB(config *config.Config) *DB {
+	db, err := gorm.Open(postgres.Open(config.DB.DSN), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return &DB{
+		DB: db,
+	}
+}

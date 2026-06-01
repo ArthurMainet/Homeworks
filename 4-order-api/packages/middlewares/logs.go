97b12032d0683fc27d45547f821532742834a894
ArturMainet <eg.molotkov@gmail.com>
2026-06-02 02:37:08 +0300

    Fix logs

--- 4-order-api/packages/middlewares/logs.go
+++ 4-order-api/packages/middlewares/logs.go
@@ -11,10 +11,11 @@ func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		method := r.Method
 		path := r.URL.Path
+		time := time.Now().Format("2006-01-02 15:04:05")
 		logrus.WithFields(logrus.Fields{
 			"Method": method,
 			"Path":   path,
-			"Time":   time.Now(),
+			"Time":   time,
 		}).Info("Логирование метода и функции.")
 		next.ServeHTTP(w, r)
 	})
